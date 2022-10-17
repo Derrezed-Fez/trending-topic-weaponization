3f5aaac78730e46372e5f27eb6fc9a9e53801dd2
Zane <zane@Zanes-MBP.hsd1.mi.comcast.net>
2022-10-17 18:18:12 -0400

    Added function for pulling the daily trends from Twitter and function comments.

--- api_wrapper.py
+++ api_wrapper.py
@@ -10,6 +10,11 @@ Class to wrap the Twitter API. Use to grab trends and perform other actions on T
 Work in Progress. Need elevated API permissions to finish implementing
 '''
 class TwitterWrapper():
+    '''
+    Init function: called when the object is instantiated.
+    Inputs: None
+    Outputs: None
+    '''
     def __init__(self):
         consumer_key = os.environ["app_key"]
         consumer_secret = os.environ["app_secret"]
@@ -24,24 +29,43 @@ class TwitterWrapper():
         )
 
         self.api = tweepy.API(auth)
-        loc = geocoder.osm("US")
-        # print(self.api.closest_trends(loc.lat, loc.lng))
-        print(self.api.available_trends())
+
+    '''
+    Init function: called whn the object is instantiated.
+    Inputs: num_trends - type: int - description: the number of trends to pull from the daily trends list
+    Outputs: the trends from the US region for the day - type: list of dictionaries
+    '''
+    def get_daily_trends(self, num_trends):
+        g = geocoder.osm("US")
+        closest_loc = self.api.closest_trends(g.lat, g.lng)
+        trends = self.api.get_place_trends(closest_loc[0]["woeid"])
+        return trends[0]["trends"][0:num_trends]
 
 '''
 A wrapper for the Google API.
 Use to grab current daily trends for Google searches.
 '''
 class GoogleWrapper():
+    '''
+    Init function: called whn the object is instantiated.
+    Inputs: None
+    Outputs: None
+    '''
     def __init__(self):
         self.api = TrendReq()
 
+    '''
+    Init function: called when the object is instantiated.
+    Inputs: num_trends - type: int - description: the number of trends to pull from the daily trends list
+    Outputs: the trends from the US region for the day - type: list of dictionaries
+    '''
     def get_daily_trends(self, num_trends):
         trends = self.api.today_searches(pn='US')
         return trends.head(num_trends)
 
-# wrapper = TwitterWrapper()
-
-wrapper = GoogleWrapper()
-# Pull the top 20 trends for the day
+wrapper = TwitterWrapper()
 print(wrapper.get_daily_trends(20))
+
+# wrapper = GoogleWrapper()
+# Pull the top 20 trends for the day
+# print(wrapper.get_daily_trends(20))
