1890700fdc5283e0c72984b6ade651dd128d5b20
rodisho <rafiodisho@gmail.com>
2022-10-23 20:43:36 -0400

    WIP - initial work for calling twitter & google api to test API call

--- api_wrapper.py
+++ api_wrapper.py
@@ -5,17 +5,21 @@ import geocoder
 import pandas as pd
 from pytrends.request import TrendReq
 import vt
+import time
 
 '''
 Class to wrap the Twitter API. Use to grab trends and perform other actions on Twitter. API Keys and other info stored in a dotenv file.
 Work in Progress. Need elevated API permissions to finish implementing
 '''
+
+
 class TwitterWrapper():
     '''
     Init function: called when the object is instantiated.
     Inputs: None
     Outputs: None
     '''
+
     def __init__(self):
         consumer_key = os.environ["app_key"]
         consumer_secret = os.environ["app_secret"]
@@ -23,10 +27,10 @@ class TwitterWrapper():
         access_token_secret = os.environ["access_secret"]
 
         auth = tweepy.OAuth1UserHandler(
-        consumer_key, 
-        consumer_secret, 
-        access_token, 
-        access_token_secret
+            consumer_key,
+            consumer_secret,
+            access_token,
+            access_token_secret
         )
 
         self.api = tweepy.API(auth)
@@ -36,22 +40,27 @@ class TwitterWrapper():
     Inputs: num_trends - type: int - description: the number of trends to pull from the daily trends list
     Outputs: the trends from the US region for the day - type: list of dictionaries
     '''
+
     def get_daily_trends(self, num_trends):
         g = geocoder.osm("US")
         closest_loc = self.api.closest_trends(g.lat, g.lng)
         trends = self.api.get_place_trends(closest_loc[0]["woeid"])
         return trends[0]["trends"][0:num_trends]
 
+
 '''
 A wrapper for the Google API.
 Use to grab current daily trends for Google searches.
 '''
+
+
 class GoogleWrapper():
     '''
     Init function: called whn the object is instantiated.
     Inputs: None
     Outputs: None
     '''
+
     def __init__(self):
         self.api = TrendReq()
 
@@ -60,20 +69,25 @@ class GoogleWrapper():
     Inputs: num_trends - type: int - description: the number of trends to pull from the daily trends list
     Outputs: the trends from the US region for the day - type: list of dictionaries
     '''
+
     def get_daily_trends(self, num_trends):
         trends = self.api.today_searches(pn='US')
         return trends.head(num_trends)
+
 
 '''
 A wrapper for the Virus Total API.
 Used to lookup malicious URLs, EXEs, and other potentially harmful vectors of attack.
 '''
+
+
 class VirusTotalWrapper():
     '''
     Init function: called whn the object is instantiated.
     Inputs: None
     Outputs: None
     '''
+
     def __init__(self):
         api_key = os.environ['virus_total_key']
         self.api = vt.client(api_key)
@@ -83,24 +97,26 @@ class VirusTotalWrapper():
     Inputs: keywords - type: list - description: the keywords to use to generate suspicious artifacts
     Outputs: A list of all artifacts that were flagged by Virus Total
     '''
+
     def get_relevant_data(self, keywords):
         urls = self.generate_urls(keywords)
         flagged = dict()
         for url in urls:
             url_id = vt.url_id(url)
             result = self.api.get_object('/urls/{}', url_id).last_analysis_statistics
+            time.sleep(15)
             if result['malicious'] > 0 or result['suspicious'] > 0:
                 flagged[url] = result
         return flagged
 
-    
     '''
     generate_urls - Used to generate a series of likely URL combinations from keywords.
     Inputs: keywords - type: list - description: the keywords to use to generate URLs
     Outputs: A list of URLs to use to query Virus Total
     '''
+
     def generate_urls(self, keywords):
-        #Both prefix and subdomains are optional, so include an empty option for each combination
+        # Both prefix and subdomains are optional, so include an empty option for each combination
         prefix_options = ['', 'http://www.', 'https://www.', 'http://com.', 'https://com.']
         postfix_options = ['.com', '.org', '.io', '.net', '.co', '.us']
         subdomain_options = ['', '/index', '/main', '/free', '/prize', '/home', '/landing']
@@ -112,8 +128,9 @@ class VirusTotalWrapper():
                 for postfix in postfix_options:
                     for subdomain in subdomain_options:
                         generated_urls.append(prefix + keyword + postfix + subdomain)
-        
+
         return generated_urls
+
 
 wrapper = TwitterWrapper()
 print(wrapper.get_daily_trends(20))
--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 import pymongo
+import time
+from api_wrapper import TwitterWrapper, GoogleWrapper, VirusTotalWrapper
 
 def main():
     # Create Mongo client and check for db intialization
-    mdb = pymongo.MongoClient("mongodb://localhost:27017/")
-    dblist = mdb.list_database_names()
-    if "topic" not in dblist:
-        topic_db = mdb['topic']
-    print(topic_db)
+    # mdb = pymongo.MongoClient("mongodb://localhost:27017/")
+    # dblist = mdb.list_database_names()
+    # if "topic" not in dblist:
+    #     topic_db = mdb['topic']
+    # print(topic_db)
+
+    twitterApi = TwitterWrapper()
+    googleApi = GoogleWrapper()
+    virusTotalApi = VirusTotalWrapper()
+
+    while True:
+        twitResults = twitterApi.get_daily_trends(5)
+        googleResult = googleApi.get_daily_trends(5)
+        # print(googleResult)
+        # print(twitResults)
+        time.sleep(5)
+
+        twitNameList = ["Russ", "Rypien", "McManus"]
+        virusReportResults = virusTotalApi.get_relevant_data(twitNameList)
+        print(virusReportResults)
+
+
+
+
+
 
 
 if __name__ == '__main__':
